f8680d86ce0026d53448fc82a9ff30a4e8607b56
LahiruPrasad12 <lahirupr471@gmail.com>
2022-08-07 18:43:39 +0530

    created midlware function to handle all the errors

---
+++ Controllers/auth_controller.js
@@ -0,0 +1,82 @@
+const User = require("../Models/user_model");
+const catchAsync = require("../Utils/catchAsync");
+const jwt = require("jsonwebtoken");
+const { promisify } = require("util");
+// const sendEmail = require("../Utils/email");
+const crypto = require("crypto");
+
+//Register new user
+exports.signup = catchAsync(async (req, res, next) => {
+    const newUser = await User.create(req.body);
+    createSendToken(newUser, 200, res);
+});
+
+
+/**Below functions are some middlewares */
+
+//created token
+const signToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+};
+
+//crate new token and send it
+const createSendToken = (user, statusCode, res) => {
+    const token = signToken(user._id);
+    const cookieOptions = {
+        expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+    };
+    if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
+
+    res.cookie("jwt", token, cookieOptions);
+
+    // Remove password from output
+    user.password = undefined;
+
+    res.status(statusCode).json({
+        status: "success",
+        token,
+        data: {
+            user,
+        },
+    });
+};
+
+
+//protected routes
+exports.protect = catchAsync(async (req, res, next) => {
+    // Getting token and check of it's there
+    let token;
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith("Bearer")
+    ) {
+        token = req.headers.authorization.split(" ")[1];
+    }
+
+    if (!token) {
+        return next(
+            new AppError("You are not logged in! Please log in to get access.", 401)
+        );
+    }
+
+    //Verification token. This will return promise. So, i jsut used promisify inbuild method
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+    // Check if user still exists
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+        return next(
+            new AppError(
+                "The user belonging to this token does no longer exist.",
+                401
+            )
+        );
+    }
+
+    // GRANT ACCESS TO PROTECTED ROUTE AND SET USER AND GROUP ID GLOBALLY
+    req.user = currentUser;
+    next();
+});
