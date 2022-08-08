396bb5a83059e9b924b6f6c208634c54d1100524
LahiruPrasad12 <lahirupr471@gmail.com>
2022-08-09 00:28:41 +0530

    created model for notice

---
+++ Models/note_model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const noticeSchema = new mongoose.Schema({
+    title: {
+        type: String,
+    },
+    description: {
+        type: String,
+    },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+});
+
+
+const Notice = mongoose.model('Notice', noticeSchema);
+module.exports = Notice;
