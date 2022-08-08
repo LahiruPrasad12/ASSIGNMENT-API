3bb17e6b0a36122e8cc6201486f868a99399707a
LahiruPrasad12 <lahirupr471@gmail.com>
2022-08-09 00:39:56 +0530

    implemnetd advance filter part and user fetch api

--- Controllers/user_controller.js
+++ Controllers/user_controller.js
@@ -1,13 +1,19 @@
 const User = require('../Models/user_model');
 const catchAsync = require('../Utils/catchAsync');
+const Filter = require('../Utils/Fliters')
 
 //get all notices
 exports.allUsers = catchAsync(async (req, res, next) => {
-    let all_users = await User.find()
-    res.status(201).json({
-        status: "success",
+    const Respond = new Filter(User.find(), req.query).filter().sort().limitFields().paginate();
+
+    const users = await Respond.query;
+
+    // SEND RESPONSE
+    res.status(200).json({
+        status: 'success',
+        results: users.length,
         data: {
-            all_users,
-        },
+            users
+        }
     });
 });
