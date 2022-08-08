2e18d6907995e6389b483ac8223b2b26e6d97fda
LahiruPrasad12 <lahirupr471@gmail.com>
2022-08-09 00:00:47 +0530

    implemented user lgout part

--- Routes/auth_routes.js
+++ Routes/auth_routes.js
@@ -5,6 +5,6 @@ const router = express.Router();
 router.post('/create-student', authController.signup);
 router.post('/signing', authController.login);
 router.patch('/update-account',authController.protect, authController.updatePassword);
-// router.get('/logout', authController.protect, authController.logout);
+router.get('/logout', authController.protect, authController.logout);
 
 module.exports = router;
