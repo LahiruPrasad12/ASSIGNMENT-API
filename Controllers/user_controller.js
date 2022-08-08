3b253e98aea63fe85f5ea47e36bbb9199e3c4601
LahiruPrasad12 <lahirupr471@gmail.com>
2022-08-09 00:37:31 +0530

    implemented find all user part

--- Controllers/user_controller.js
+++ Controllers/user_controller.js
@@ -1,2 +1,13 @@
 const User = require('../Models/user_model');
 const catchAsync = require('../Utils/catchAsync');
+
+//get all notices
+exports.allUsers = catchAsync(async (req, res, next) => {
+    let all_users = await User.find()
+    res.status(201).json({
+        status: "success",
+        data: {
+            all_users,
+        },
+    });
+});
