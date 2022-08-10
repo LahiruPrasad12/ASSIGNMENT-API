85e5e4d8af371a459e51cc66acf7185528b5896a
LahiruPrasad12 <lahirupr471@gmail.com>
2022-08-10 11:43:49 +0530

    implemented rest api for get current user

--- Routes/auth_routes.js
+++ Routes/auth_routes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.post('/signing', authController.login);
 router.patch('/update-account',authController.protect, authController.updateMyAccount);
+router.get('/current-user', authController.protect, authController.currentUser);
 router.get('/logout', authController.protect, authController.logout);
 
 module.exports = router;
